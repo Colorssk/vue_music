28098ab33637a2699ee1822ac41a8df55b97e5fb
Colorssk <37653722+Colorssk@users.noreply.github.com>
2018-04-26 17:31:37 +0800

    vue_music（vue全家桶）

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li @click="selectItem(song, index)" class="item" v-for="(song, index) in songs">
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{getDesc(song)}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      songs: {
+        type: Array,
+        default: []
+      }
+    },
+    methods: {
+      selectItem(item, index) {
+        this.$emit('select', item, index)
+      },
+      getDesc(song) {
+        return `${song.singer}·${song.album}`
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .song-list
+    .item
+      display: flex
+      align-items: center
+      box-sizing: border-box
+      height: 64px
+      font-size: $font-size-medium
+      .content
+        flex: 1
+        line-height: 20px
+        overflow: hidden
+        .name
+          no-wrap()
+          color: $color-text
+        .desc
+          no-wrap()
+          margin-top: 4px
+          color: $color-text-d
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import store from './store'
+
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
