4283623d3774d6de2df57d2498747d5fee8cb04d
Colorssk <37653722+Colorssk@users.noreply.github.com>
2018-05-05 16:34:09 +0800

    实现搜索功能

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -24,6 +24,14 @@
       data: {
         type: Array,
         default: null
+      },
+      pullup: {
+        type: Boolean,
+        default: false
+      },
+      beforeScroll: {
+        type: Boolean,
+        default: false
       }
     },
     mounted() {
@@ -45,6 +53,20 @@
           let me = this
           this.scroll.on('scroll', (pos) => {
             me.$emit('scroll', pos)
+          })
+        }
+
+        if (this.pullup) {
+          this.scroll.on('scrollEnd', () => {
+            if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+              this.$emit('scrollToEnd')
+            }
+          })
+        }
+
+        if (this.beforeScroll) {
+          this.scroll.on('beforeScrollStart', () => {
+            this.$emit('beforeScroll')
           })
         }
       },
---
+++ src/base/search-list/search-list.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search-list" v-show="searches.length">
+    <ul>
+      <li class="search-item" @click="selectItem(item)" v-for="item in searches">
+        <span class="text">{{item}}</span>
+        <span class="icon" @click.stop="deleteOne(item)">
+          <i class="icon-delete"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      searches: {
+        type: Array,
+        default: []
+      }
+    },
+    methods: {
+      selectItem(item) {
+        this.$emit('select', item)
+      },
+      deleteOne(item) {
+        this.$emit('delete', item)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .search-list
+    .search-item
+      display: flex
+      align-items: center
+      height: 40px
+      .text
+        flex: 1
+        color: $color-text-l
+      .icon
+        extend-click()
+        .icon-delete
+          font-size: $font-size-small
+          color: $color-text-d
+</style>
